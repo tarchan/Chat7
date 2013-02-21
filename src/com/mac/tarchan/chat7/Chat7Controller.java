de4fb611dba9b2ab32fc36e2ca13f32ed9b35461
tarchan <tarchan@gmail.com>
2013-02-21 18:50:06 +0900

    進捗状況表示を追加

--- src/com/mac/tarchan/chat7/Chat7Controller.java
+++ src/com/mac/tarchan/chat7/Chat7Controller.java
@@ -22,6 +22,7 @@ import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
+import javafx.application.Platform;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.concurrent.Service;
@@ -30,8 +31,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 
@@ -42,8 +45,16 @@ import javafx.stage.FileChooser;
 public class Chat7Controller implements Initializable {
 
     private static final Logger log = Logger.getLogger(Chat7Controller.class.getName());
+
+    static {
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(new SimpleFormatter());
+        handler.setLevel(Level.CONFIG);
+        log.setLevel(Level.CONFIG);
+        log.setUseParentHandlers(false);
+        log.addHandler(handler);
+    }
     private FileChooser fileChooser = new FileChooser();
-//    private File file;
     private SimpleObjectProperty<File> file = new SimpleObjectProperty<>(this, "file");
     private ReadLogService readLogService = new ReadLogService();
     @FXML
@@ -54,15 +65,10 @@ public class Chat7Controller implements Initializable {
     private TextField input;
     @FXML
     private Button send;
-
-    static {
-        ConsoleHandler handler = new ConsoleHandler();
-        handler.setFormatter(new SimpleFormatter());
-        handler.setLevel(Level.CONFIG);
-        log.setLevel(Level.CONFIG);
-        log.setUseParentHandlers(false);
-        log.addHandler(handler);
-    }
+    @FXML
+    private Region glass;
+    @FXML
+    private ProgressIndicator loading;
 
     public Property<File> fileProperty() {
         return file;
@@ -85,12 +91,26 @@ public class Chat7Controller implements Initializable {
         }
     }
 
+    @FXML
+    private void handleConnect(ActionEvent e) {
+        log.info("IRCに接続します。");
+    }
+
+    @FXML
+    private void handleExit(ActionEvent e) {
+        log.log(Level.INFO, "アプリケーションを終了します。");
+        Platform.exit();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         FileChooser.ExtensionFilter allFilter = new FileChooser.ExtensionFilter("すべてのファイル (*.*)", "*.*");
         fileChooser.getExtensionFilters().add(allFilter);
 
         console.textProperty().bind(readLogService.valueProperty());
+        glass.visibleProperty().bind(readLogService.runningProperty());
+        loading.visibleProperty().bind(readLogService.runningProperty());
+        loading.progressProperty().bind(readLogService.progressProperty());
     }
 
     private void openFile(Path path) {
